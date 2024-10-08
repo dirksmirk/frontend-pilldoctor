9ef3155a2f7f7c6b543578f09f22a65ab5a44c27
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2024-10-08 21:57:30 +0200

    Trying to use Homepage, component library not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,11 @@
 import { Login, Homepage, Register, Calendar } from "pilldoctor-components";
+import "./App.css";
+
+const App = () => {
+
+    return (
+        <Homepage />
+    );
+};
+
+export default App;
